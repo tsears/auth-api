646e66a1ef3f5d67f6eba44cfa764b72c0ea87f6
Tom Sears <tom@tsears.net>
2017-05-21 22:09:31 -0700

    Add missing file

---
+++ app/settings.js
@@ -0,0 +1,16 @@
+module.exports = function(env) {
+  let s;
+  if (!s) {
+    s = {
+      port: env.APP_PORT,
+      mongoIP: env.MONGODB_IP,
+      mongoPort: env.MONGODB_PORT,
+      mongoDatabase: env.MONGODB_DATABASE,
+      sessionSecret: env.SESSION_SECRET,
+      adminUser: env.ADMIN_USER,
+      adminPass: env.ADMIN_PASS,
+    }
+  }
+
+  return s;
+}
